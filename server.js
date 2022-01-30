b098698785903e3d6c39714b4cb2a28f481c91c2
mannu <mannu.singh@habilelabs.io>
2022-01-30 08:35:38 +0530

    server.js added

---
+++ server.js
@@ -0,0 +1,10 @@
+// by using framewok express
+const express = require('express');  // requires express module
+const app = express(); // instance of express
+
+app.get("/",(req,res) => { //declare the route here
+    res.send("defaut route is called");
+})
+app.listen(8080, function () { // listening 
+    console.log("server runs at port: 8080");
+});
